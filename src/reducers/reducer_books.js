d736b2274012d6a57f76798bcb25a14585aea7b3
Scott Streit <Scott.Streit@newellco.com>
2018-04-02 17:57:50 -0400

    Section 4 Lecture 45

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Javascript: The Good Parts'},
+        { title: 'Borne Ultimatium'},
+        { title: 'Borne Supremecy'},
+        { title: 'The Hunt for Red October'}
+    ]
+}
